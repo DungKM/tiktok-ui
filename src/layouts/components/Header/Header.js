8265fcf479ef62684acc8ea4c4d267cf7b84eb3e
Hoàng Anh Dũng <108377940+DungKM@users.noreply.github.com>
2023-09-08 08:56:49 +0700

    Ex15.Optimization File index

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,153 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+import images from "~/assets/images";
+import {
+  faCircleQuestion,
+  faCoins,
+  faEarthAsia,
+  faEllipsisVertical,
+  faGear,
+  faKeyboard,
+  faSignOut,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
+import Button from "~/components/Button";
+import Menu from "~/components/Propper/Menu";
+import { InboxIcon, MessageIcon, UploadIcon } from "~/components/Icon";
+import Image from "~/components/Image";
+import Search from "../Search";
+import { Link } from "react-router-dom";
+import { config } from "~/config";
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: "English",
+    children: {
+      title: "Language",
+      data: [
+        {
+          type: "Language",
+          code: "en",
+          title: "English",
+        },
+        {
+          type: "Language",
+          code: "vi",
+          title: "Tiếng Việt",
+        },
+      ],
+    },
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: "Feedback and help",
+    to: "/feedback",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: "Keyboard shortcuts",
+  },
+];
+
+function Header() {
+ 
+  // Handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case "language":
+        break;
+      default:
+    }
+  };
+  const currentUser = true;
+
+  const userMenu = [
+    {
+      icon: <FontAwesomeIcon icon={faUser} />,
+      title: "View profile",
+      to: "/@hoa",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faCoins} />,
+      title: "Get coins",
+      to: "/coin",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faGear} />,
+      title: "Setting",
+      to: "/setting",
+    },
+    ...MENU_ITEMS,
+    {
+      icon: <FontAwesomeIcon icon={faSignOut} />,
+      title: "Logout",
+      to: "/logout",
+      separate: true,
+    },
+  ];
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <Link to={config.routes.home} className={cx("logo-link")}>
+          <img src={images.logo} alt="Tiktok" />
+        </Link>
+        {/* Search */}
+        <Search />
+
+        {/* action */}
+        <div className={cx("action")}>
+          {currentUser ? (
+            <>
+              <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <UploadIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Tin nhắn" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <MessageIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Hộp thư" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <sup className={cx("sup-inbox")}>20</sup>
+                  <InboxIcon />
+                </button>
+              </Tippy>
+            </>
+          ) : (
+            <>
+              <Button text>Upload</Button>
+              {/* <Button primary lefticon={ <FontAwesomeIcon icon={faSignIn} />}>Log in</Button> */}
+              <Button primary>Log in</Button>
+            </>
+          )}
+          <Menu
+            items={currentUser ? userMenu : MENU_ITEMS}
+            onChange={handleMenuChange}
+          >
+            {currentUser ? (
+              <Image
+                className={cx("user-avatar")}
+                alt="Hoang Anh Dung"
+                src="httpp16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-aiso/f380dc95682d4a49aff9396b638edee1~c5_300x300.webp?x-expires=1693699200&x-signature=ErudhL5SPLFRjQvkuZNjRgC%2FWvo%3D"
+                fallback="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e3be7b235b902fd85cd9ed42b58ab5a~c5_100x100.jpeg?x-expires=1694052000&x-signature=gwFgdIaW4ZToHFtje12XmF0TYlE%3D"
+              />
+            ) : (
+              <button className={cx("more-btn")}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
+          </Menu>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
